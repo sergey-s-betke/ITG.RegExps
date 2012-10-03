d0683a2990a1174ce8b03b135e7f5c68282314a4
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-03 17:03:42 +0400

    Первая редакция в GIT
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

---
+++ ITG.RegExps.psm1
@@ -0,0 +1,7 @@
+﻿# регулярное выражение для выделения адреса электронной почты 
+[System.Text.RegularExpressions.Regex]$reDomain = "(?<domain>(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-zA-Z]{2}))"
+[System.Text.RegularExpressions.Regex]$reMailAddr = "(?<addr>(?<lname>[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*)@$($reDomain))"
+
+Export-ModuleMember -variable `
+    reDomain `
+    ,reMailAddr
