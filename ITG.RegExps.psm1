18f8601b4bd4bf12d3471324d96001b8909963a8
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-12-20 22:45:59 +0400

    + добавлены регулярные выражения для разбора URL
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

--- ITG.RegExps.psm1
+++ ITG.RegExps.psm1
@@ -1,7 +1,47 @@
-﻿# регулярное выражение для выделения адреса электронной почты 
-[System.Text.RegularExpressions.Regex]$reDomain = "(?<domain>(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-zA-Z]{2}))"
-[System.Text.RegularExpressions.Regex]$reMailAddr = "(?<addr>(?<lname>[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*)@$($reDomain))"
+﻿[System.Text.RegularExpressions.Regex]$reDomain = `
+	"(?<domain>(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-zA-Z]{2}))";
+[System.Text.RegularExpressions.Regex]$reMailAddr = `
+	"(?<addr>(?<lname>[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*)@$($reDomain))";
 
-Export-ModuleMember -variable `
-    reDomain `
-    ,reMailAddr
+[System.Text.RegularExpressions.Regex]$rePort = `
+	"(?:[:](?<port>\d+))";
+[System.Text.RegularExpressions.Regex]$reSocket = `
+	"(?<socket>${reDomain}${rePort}?)";
+[System.Text.RegularExpressions.Regex]$reSchema = `
+	"(?<schema>http|https|ftp)";
+[System.Text.RegularExpressions.Regex]$reURLToken = `
+	"(?:(?:%[0-9a-fA-F]{2})|[a-zA-Z0-9!$'*+^_`{|}~.`-]|&[a-z]+;)";
+[System.Text.RegularExpressions.Regex]$reURLPathEl = `
+	"${reURLToken}+";
+[System.Text.RegularExpressions.Regex]$reURLPath = `
+	"(?<path>${reURLPathEl}(?:/${reURLPathEl})*)";
+[System.Text.RegularExpressions.Regex]$reURLParam = `
+	"(?<paramName>${reURLToken}+)=(?<paramValue>${reURLToken}+)";
+[System.Text.RegularExpressions.Regex]$reURLParams = `
+	"(?:(?:[?])(?<params>${reURLParam}(?:&${reURLParam})*))";
+[System.Text.RegularExpressions.Regex]$reURLAnchor = `
+	"(?:(?:#)(?<anchor>${reURLToken}+))";
+[System.Text.RegularExpressions.Regex]$reURL = `
+	"(?<url>${reSchema}://${reDomain}${rePort}?(?:/${reURLPath}?${reURLParams}?${reURLAnchor}?)?)";
+[System.Text.RegularExpressions.Regex]$reURLShortHTTP = `
+	"(?<url>(?<domain>(?:www)`.${reDomain})${rePort}?(?:/${reURLPath}?${reURLParams}?${reURLAnchor}?)?)";
+[System.Text.RegularExpressions.Regex]$reURLShortFTP = `
+	"(?<url>(?<domain>(?:ftp)`.${reDomain})${rePort}?(?:/${reURLPath}?${reURLParams}?${reURLAnchor}?)?)";
+
+Export-ModuleMember `
+	-Variable `
+		  reDomain `
+		, reMailAddr `
+		, rePort `
+		, reSocket `
+		, reSchema `
+		, reURLToken `
+		, reURLPathEl `
+		, reURLPath `
+		, reURLParam `
+		, reURLParams `
+		, reURLAnchor `
+		, reURL `
+		, reURLShortHTTP `
+		, reURLShortFTP `
+;
